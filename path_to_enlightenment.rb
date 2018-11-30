43bf7d290edda7f8f762dd6595ca00ac7f5abbe7
Mu'men Yassin <yassin.mumen@gmail.com>
2018-12-01 01:24:43 +0200

    progress

--- path_to_enlightenment.rb
+++ path_to_enlightenment.rb
@@ -19,7 +19,7 @@ require 'about_constants' # DONE
 require 'about_control_statements' # DONE
 require 'about_true_and_false' # DONE
 require 'about_triangle_project' # DONE
-require 'about_exceptions'
+require 'about_exceptions' # DONE
 require 'about_triangle_project_2'
 require 'about_iteration'
 require 'about_blocks'
